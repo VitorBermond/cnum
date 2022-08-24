ddc6ab37132a459b92615f12e29484ae5887674f
VitorBermond <111003425+VitorBermond@users.noreply.github.com>
2022-08-24 17:31:18 +0000

    terminando ex  3.2.8

--- 3/main.jl
+++ 3/main.jl
@@ -1,6 +1,8 @@
 f1(x) = sqrt(x) - cos(x)                       #3.2.1
 f2(x) = 5*sin(x^2) - MathConstants.e^(x/10)    #3.2.2
-f3(x) = x+1     #3.2.8
+
+vt = ((1.380649 * 10^-23)*300)/(1.6 * 10^-19)
+f3(x) = (10^-12) * (MathConstants.e^(x/vt) - 1)                    #3.2.8
 
 erro = 10^-5
 
@@ -17,8 +19,12 @@ function bissecao(a,b,f)
   return x
 end
 
+println("\n 3.2.2 \n")
+
 r = bissecao(0, 1, f1)
 println(r)
+
+println("\n 3.2.2 \n")
 
 r = bissecao(0, 1, f2)
 println(r)
@@ -27,5 +33,9 @@ println(r)
 r = bissecao(2, 2.002, f2)
 println(r)
 
-r = bissecao(0, 1, f3)
+println("\n 3.2.8 \n")
+
+
+
+r = bissecao(-2, 1, f3)
 println(r)
