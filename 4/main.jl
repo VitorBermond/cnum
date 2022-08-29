937d21c33d1de5ce495a9fef914b139a864fc4d5
VitorBermond <111003425+VitorBermond@users.noreply.github.com>
2022-08-29 19:16:09 +0000

    Adicionado aula 4

--- 4/main.jl
+++ 4/main.jl
@@ -1,12 +1,23 @@
-function bisection(a,b)
-  x = (a+b)/2
-  while abs(f(x)) > error
-    if f(a)*(fx) > 0
-      a = x
-    else
-      b = x
-    end
-    x = (a+b)/2
-  end 
-  return x 
-end 
+using .MathConstants:e
+
+error = 0.0001
+
+f1(x) = (e^x)-x-2
+
+function fixedpoint(a,g)
+  x=g(a)
+  while abs(x-a) > error
+    a = x
+    x = g(a)
+  end
+  return x
+end
+
+println(" Ex 3.3.1")
+
+g(x) = (e^x)-2
+r = fixedpoint(1,g)
+println(r)
+
+println(" Ex 3.4.1")
+
